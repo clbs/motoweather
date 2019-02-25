9bdeaefe52d4366844e770f8488711d126c8a64d
Caleb <caleb.coates@gmail.com>
2019-02-25 00:08:51 -0500

    Add files via upload
    fixed issue with logo div covering controls on mobile

---
+++ build/precache-manifest.ebd8d248eb672f508b87d119199c9081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "19b4ea3e22326934be83",
+    "url": "/static/css/main.0cde82c3.chunk.css"
+  },
+  {
+    "revision": "19b4ea3e22326934be83",
+    "url": "/static/js/main.19b4ea3e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "cb2d76cb4f838bbe717c7777b87b0a27",
+    "url": "/static/media/background.cb2d76cb.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1957b144458bc723a5fca99099010e3",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80dc05db2c5ee7caf966bbfc937d1fca.js"
+  "/precache-manifest.ebd8d248eb672f508b87d119199c9081.js"
 );
 
 workbox.clientsClaim();
